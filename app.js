22ad037179c342a44fdec61fb496127124b43b0d
David <ds90289@yahoo.com>
2020-11-25 00:09:37 +0800

    added socket.io, changed filestructure.

--- app.js
+++ app.js
@@ -4,35 +4,45 @@ const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+var bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-server.listen(port, () => {
-  if(port == 3000)
-    console.log("Server is running on 'http://localhost:%d/'.", port);
-  else
-  console.log("Server is running on port %d.", port);
-});
-
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.text({ type: 'text/html' }))
+//app.use(bodyParser.json({ type: 'application/*+json' }))
 
 let numUsers = 0;
 let clientList = [];
 
-app.get('/', (req, res) => {
+io.on('connection', (client) => { 
+  require('./services/loungeService.js')(io, client);
+  require('./services/lobbyService.js')(io, client);
+  require('./services/gameService.js')(io, client);
+  
+});
+
+app.get('/', (req, res, next) => {
   res.render('index.html');
 });
 
-app.get('/auth', (req, res) => {
+app.get('/auth', function (req, res) {
   // validate user
-  console.log(req.params.loginName);
+  console.log(req.query);
+  console.log(req.query.loginName);
   res.redirect('/authenticated');
 });
 
 app.get('/authenticated', (req, res, next) => {
+  console.log(req.query.loginName);
   res.render("authenticated/lounge.html");
 });
 
@@ -40,3 +50,9 @@ app.get('/lobby', (req, res, next) => {
   res.render("authenticated/lobby.html");
 });
 
+server.listen(port, () => {
+  if(port == 3000)
+    console.log("Server is running on 'http://localhost:%d/'.", port);
+  else
+  console.log("Server is running on port %d.", port);
+});
