bdbf9eaee705541856b33687217c17466c6d2170
jdinkelbach <jmdinkelbach@gmail.com>
2020-11-29 01:58:57 +0000

    ready button click listener

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,3 +1,20 @@
+/* global $ */
+/* global io */
+
 $(document).ready(function() {
 	var socket = io();
+	
+	// Ready Button
+	$("#readyBtn").on("click", function(){
+	    if ($(this).hasClass("btn-info")){
+	        $(this).removeClass("btn-info");
+	        $(this).addClass("btn-dark");
+	        $(this).html(`Ready <i class="fas fa-check pl-2"></i>`);
+	    }
+	    else{
+	        $(this).removeClass("btn-dark");
+	        $(this).addClass("btn-info");
+	        $(this).html(`Not Ready`);
+	    }
+	})
 });
