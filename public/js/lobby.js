7bbd9373bd060f528e7286f5e1f3092c8e66fca4
jdinkelbach <jmdinkelbach@gmail.com>
2020-11-29 02:12:35 +0000

    sendMessage button click listener

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -29,4 +29,38 @@ $(document).ready(function() {
 	    $("#lobbyPanel").hide();
 	    $("#chatPanel").show();
 	})
+	
+	// chat functions begin
+	$("#messageSend").click( function() {
+		var message = $("#messageInput").val(); // get message
+		$("#messageInput").val("");
+		socket.emit('stop typing');
+
+		if(message == "")
+			return;
+
+		socket.emit('new message', message);
+
+		var avatar = document.createElement("img");
+		avatar.setAttribute("src", "../img/avatar-male.jpg");
+		avatar.setAttribute("class", "avatar-margin");
+		avatar.setAttribute("alt", "avatar");
+		avatar.setAttribute("width", "25px");
+		avatar.setAttribute("height", "25px");
+
+		var span = document.createElement("span");
+		// span.setAttribute("class", "app-message-sent");
+		span.append(message);
+
+		var div = document.createElement("div");
+		// div.setAttribute("class", "sent-message-wrapper");
+		div.append(avatar);
+		div.append(span);
+
+		var item = document.createElement("li");
+		item.setAttribute("class", "list-item");
+		item.append(div);
+
+		$("#messageList").append(item);
+	});
 });
