d11cf1ce1c432d8f9cda92f36382280dfdc368db
SuperGato <ledbrandon@yahoo.com>
2020-11-30 12:10:14 -0800

    working on collision bug

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,7 @@
 import Circle from "./GameObjects/Circle.js"
-import {drawCircle} from "./GameFunctions/drawCircle.js";
-import {controller} from "./GameFunctions/controller.js";
+import {drawCircle} from "./GameFunctions/drawCircle.js"
+import {controller} from "./GameFunctions/controller.js"
+import {checkCollision} from "./GameFunctions/checkCollision.js"
 
 console.log("game.js connected");
 
@@ -16,22 +17,18 @@ var circle2 = new Circle(120,50,10,0, "yellow",0);
 var step = function() {
 	controller(circle1);
 	ctx.clearRect(0,0,screenWidth,screenHeight);
+	var tempx = circle1.x;
+	var tempy = circle1.y;
 	if (checkCollision(circle1, circle2)) {
 		console.log("collided");
-		circle1.x = circle2.x - circle2.rad * 2 - 1;
+		circle1.move = 0;
+		circle1.x = tempx - 2;
+		circle1.y = tempy - 2;
+		circle1.move = 2;
 	}
 	drawCircle(circle1, ctx);
 	drawCircle(circle2, ctx);
 	window.requestAnimationFrame(step);
-}
-
-var checkCollision = function (circle1, circle2) {
-	let cir1x2 = circle1.x + circle1.rad*2;
-	let cir2x2 = circle2.x + circle2.rad*2;
-	let cir1y2 = circle1.y + circle1.rad*2;
-	let cir2y2 = circle1.y + circle1.rad*2;
-
-	return circle1.x < cir2x2 && cir1x2 > circle2.x && circle1.y < cir2y2 && cir1y2 > circle2.y;
 }
 
 step();
