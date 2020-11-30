3cdeb69c402aacfc9e0333a8b64f6bdcc8646661
SuperGato <ledbrandon@yahoo.com>
2020-11-30 15:29:24 -0800

    Smaller circles and change color on touched

--- public/js/game.js
+++ public/js/game.js
@@ -11,8 +11,8 @@ var ctx = canvas.getContext('2d');
 let screenWidth = 1000;
 let screenHeight = 500;
 
-var circle1 = new Circle(50,50,10,0, "white",0);
-var circle2 = new Circle(80,50,10,0, "yellow",0);
+var circle1 = new Circle(50,50,5,0, "white",0);
+var circle2 = new Circle(80,50,5,0, "yellow",0);
 
 var step = function() {
 	controller(circle1);
@@ -21,6 +21,7 @@ var step = function() {
 		circle1.speed = -circle1.speed;
 		circle1.x += circle1.speed;
 		circle1.y += circle1.speed;
+		circle2.color = circle1.color;
 	}
 	drawCircle(circle1, ctx);
 	drawCircle(circle2, ctx);
