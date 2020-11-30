40ce7a63c4df4390ac47c7b91d698501beff0d9d
SuperGato <ledbrandon@yahoo.com>
2020-11-29 22:54:44 -0800

    Testing collision

--- public/js/game.js
+++ public/js/game.js
@@ -16,9 +16,22 @@ var circle2 = new Circle(120,50,10,0, "yellow",0);
 var step = function() {
 	controller(circle1);
 	ctx.clearRect(0,0,screenWidth,screenHeight);
+	if (checkCollision(circle1, circle2)) {
+		console.log("collided");
+		circle1.x = circle2.x - circle2.rad * 2 - 1;
+	}
 	drawCircle(circle1, ctx);
 	drawCircle(circle2, ctx);
 	window.requestAnimationFrame(step);
+}
+
+var checkCollision = function (circle1, circle2) {
+	let cir1x2 = circle1.x + circle1.rad*2;
+	let cir2x2 = circle2.x + circle2.rad*2;
+	let cir1y2 = circle1.y + circle1.rad*2;
+	let cir2y2 = circle1.y + circle1.rad*2;
+
+	return circle1.x < cir2x2 && cir1x2 > circle2.x && circle1.y < cir2y2 && cir1y2 > circle2.y;
 }
 
 step();
