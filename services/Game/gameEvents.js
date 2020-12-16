ed9a6f3d145ef3e719e34c4914743c23f61e687f
EC2 Default User <ec2-user@ip-172-31-1-191.us-west-1.compute.internal>
2020-12-16 05:58:35 +0000

    why cant I commit

--- services/Game/gameEvents.js
+++ services/Game/gameEvents.js
@@ -1,103 +1,114 @@
-// const { gameSessions } = require('../gameSession');
+const { gameSessions } = require('../gameSession');
 
+module.exports.playerCollisionEvent = function(collider, collidee, ruleset) {
 
-(function() {
+	// console.log(`${collider.name} has collided with ${collidee.name} in a ${ruleset} game.`);
+	var gameSessionManager = require('../gameSession');
 
-	module.exports.playerCollisionEvent = function(collider, collidee, ruleset) {
-
-		// console.log(`${collider.name} has collided with ${collidee.name} in a ${ruleset} game.`);
-		var gameSessionManager = require('../gameSession');
-
-		switch(ruleset)
-		{
-			case "lastman":
-				if(collider.isHot === true)
-				{
-					collidee.color = "grey";
-					collidee.isFrozen = true;
-					const gameSessionID = gameSessionManager.WhereIsPlayer(collidee.name);
-					gameSessionManager.UpdatePlayer(gameSessionID, collidee);
-				}
-				else
-				{
-					// console.log("Collider is not hot");
-				}
-			break;
-
-			case "infection":
-				if(collider.isHot === true)
-				{
-					collidee.isHot = true;
-					collidee.isFast = true;
-					collidee.color = "white";
-					const gameSessionID = gameSessionManager.WhereIsPlayer(collider.name);
-					gameSessionManager.UpdatePlayer(gameSessionID, collidee);
-				}
-				else
-				{
-					// console.log("Collider is not hot");
-				}
-
-			break;
-		}
-		
-		module.exports.GameEnd = function(gameSessionID) {
-
-			var gameSessionManager = require('../gameSession');
-
-			var players = gameSessionManager.GetAllPlayers(gameSessionID);
-			var options = gameSessionManager.GetOptions(gameSessionID);
-
-			var gameResults = { "winners": [], "losers": [] };
-
-			// get the predators and prey.
-			var activePredators = players.filter(player => {
-				return player.isHot;
-			});
-
-			var activePrey = players.filter(player => {
-				return !player.isFrozen && !player.isHot; // people who are neither frozen nor hot
-			});
-
-
-			var inactivePrey = players.filter(player => {
-				return player.isFrozen && !player.isHot;
-			})
-
-			switch(options.ruleset)
+	switch(ruleset)
+	{
+		case "lastman":
+			if(collider.isHot === true)
 			{
-				case "lastman":
-					if(activePrey.length > 0)
-					{
-						gameResults.winners = activePrey;
-						gameResults.losers = activePredators.concat(inactivePrey);
-					}
-					else
-					{
-						gameResults.winners = activePredators;
-						gameResults.losers = activePrey.concat(inactivePrey);
-					}
-					
-					break;
+				collidee.color = "grey";
+				collidee.isFrozen = true;
+				const gameSessionID = gameSessionManager.WhereIsPlayer(collidee.name);
+				gameSessionManager.UpdatePlayer(gameSessionID, collidee);
+			}
+			else
+			{
+				// console.log("Collider is not hot");
+			}
+		break;
 
-				case "infection":
-					if(activePrey.length > 0)
-					{
-						gameResults.winners = activePrey;
-						gameResults.losers = activePredators;
-					}
-					else
-					{
-						gameResults.winners = activePredators;
-						gameResults.losers = activePrey;
-					}
-					break;
+		case "infection":
+			if(collider.isHot === true)
+			{
+				collidee.isHot = true;
+				collidee.isFast = true;
+				collidee.color = "white";
+				const gameSessionID = gameSessionManager.WhereIsPlayer(collider.name);
+				gameSessionManager.UpdatePlayer(gameSessionID, collidee);
+			}
+			else
+			{
+				// console.log("Collider is not hot");
 			}
 
-			console.log(gameResults);
+		break;
+	}
+	
+	module.exports.GameEnd = function(gameSessionID) {
 
-			// save in the db here //
+		var gameSessionManager = require('../gameSession');
+
+		var players = gameSessionManager.GetAllPlayers(gameSessionID);
+		var options = gameSessionManager.GetOptions(gameSessionID);
+
+		var gameResults = { "winners": [], "losers": [] };
+
+		// get the predators and prey.
+		var activePredators = players.filter(player => {
+			return player.isHot;
+		});
+
+		var activePrey = players.filter(player => {
+			return !player.isFrozen && !player.isHot; // people who are neither frozen nor hot
+		});
+
+		var inactivePrey = players.filter(player => {
+			return player.isFrozen && !player.isHot;
+		})
+
+		switch(options.ruleset)
+		{
+			case "lastman":
+				if(activePrey.length > 0)
+				{
+					gameResults.winners = activePrey;
+					gameResults.losers = activePredators.concat(inactivePrey);
+				}
+				else
+				{
+					gameResults.winners = activePredators;
+					gameResults.losers = activePrey.concat(inactivePrey);
+				}
+				
+				break;
+
+			case "infection":
+				if(activePrey.length > 0)
+				{
+					gameResults.winners = activePrey;
+					gameResults.losers = activePredators;
+				}
+				else
+				{
+					gameResults.winners = activePredators;
+					gameResults.losers = activePrey;
+				}
+				break;
 		}
 
-	};
-}());
+		console.log(gameResults);
+
+		// save in the db here //
+			// save in the db here //
+		var sql = require('../mysqlService');
+		gameResults.winners.forEach((player, i) => {
+         sql.updateUserStats(player.name, true, function(results){
+         	console.log(results + "The game data has been inserted into the DB");
+         })//callback function
+      });//for each
+      
+      gameResults.losers.forEach((player, i) => {
+         sql.updateUserStats(player.name, false, function(results){
+         	console.log(results + "The game data has been inserted into the DB");
+         })//callback function
+      });//for each
+      
+      sql.updateMapStats(options.map);
+      
+	}
+
+};
