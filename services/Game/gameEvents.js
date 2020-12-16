926baa46e175bf5f9420a1b54b86bc6ab8089f88
David <ds90289@yahoo.com>
2020-12-16 13:28:50 +0800

    gameEvent.js was missing brackets...

--- services/Game/gameEvents.js
+++ services/Game/gameEvents.js
@@ -1,98 +1,102 @@
-const { gameSessions } = require('../gameSession');
+// const { gameSessions } = require('../gameSession');
 
-module.exports.playerCollisionEvent = function(collider, collidee, ruleset) {
 
-	// console.log(`${collider.name} has collided with ${collidee.name} in a ${ruleset} game.`);
-	var gameSessionManager = require('../gameSession');
+(function() {
 
-	switch(ruleset)
-	{
-		case "lastman":
-			if(collider.isHot === true)
-			{
-				collidee.color = "grey";
-				collidee.isFrozen = true;
-				const gameSessionID = gameSessionManager.WhereIsPlayer(collidee.name);
-				gameSessionManager.UpdatePlayer(gameSessionID, collidee);
-			}
-			else
-			{
-				// console.log("Collider is not hot");
-			}
-		break;
+	module.exports.playerCollisionEvent = function(collider, collidee, ruleset) {
 
-		case "infection":
-			if(collider.isHot === true)
-			{
-				collidee.isHot = true;
-				collidee.isFast = true;
-				collidee.color = "white";
-				const gameSessionID = gameSessionManager.WhereIsPlayer(collider.name);
-				gameSessionManager.UpdatePlayer(gameSessionID, collidee);
-			}
-			else
-			{
-				// console.log("Collider is not hot");
-			}
-
-		break;
-	}
-	
-	module.exports.GameEnd = function(gameSessionID) {
-
+		// console.log(`${collider.name} has collided with ${collidee.name} in a ${ruleset} game.`);
 		var gameSessionManager = require('../gameSession');
 
-		var players = gameSessionManager.GetAllPlayers(gameSessionID);
-		var options = gameSessionManager.GetOptions(gameSessionID);
-
-		var gameResults = { "winners": [], "losers": [] };
-
-		// get the predators and prey.
-		var activePredators = players.filter(player => {
-			return player.isHot;
-		});
-
-		var activePrey = players.filter(player => {
-			return !player.isFrozen && !player.isHot; // people who are neither frozen nor hot
-		});
-
-		var inactivePrey = players.filter(player => {
-			return player.isFrozen && !player.isHot;
-		})
-
-		switch(options.ruleset)
+		switch(ruleset)
 		{
 			case "lastman":
-				if(activePrey.length > 0)
+				if(collider.isHot === true)
 				{
-					gameResults.winners = activePrey;
-					gameResults.losers = activePredators.concat(inactivePrey);
+					collidee.color = "grey";
+					collidee.isFrozen = true;
+					const gameSessionID = gameSessionManager.WhereIsPlayer(collidee.name);
+					gameSessionManager.UpdatePlayer(gameSessionID, collidee);
 				}
 				else
 				{
-					gameResults.winners = activePredators;
-					gameResults.losers = activePrey.concat(inactivePrey);
+					// console.log("Collider is not hot");
 				}
-				
-				break;
+			break;
 
 			case "infection":
-				if(activePrey.length > 0)
+				if(collider.isHot === true)
 				{
-					gameResults.winners = activePrey;
-					gameResults.losers = activePredators;
+					collidee.isHot = true;
+					collidee.isFast = true;
+					collidee.color = "white";
+					const gameSessionID = gameSessionManager.WhereIsPlayer(collider.name);
+					gameSessionManager.UpdatePlayer(gameSessionID, collidee);
 				}
 				else
 				{
-					gameResults.winners = activePredators;
-					gameResults.losers = activePrey;
+					// console.log("Collider is not hot");
 				}
-				break;
+
+			break;
+		}
+		
+		module.exports.GameEnd = function(gameSessionID) {
+
+			var gameSessionManager = require('../gameSession');
+
+			var players = gameSessionManager.GetAllPlayers(gameSessionID);
+			var options = gameSessionManager.GetOptions(gameSessionID);
+
+			var gameResults = { "winners": [], "losers": [] };
+
+			// get the predators and prey.
+			var activePredators = players.filter(player => {
+				return player.isHot;
+			});
+
+			var activePrey = players.filter(player => {
+				return !player.isFrozen && !player.isHot; // people who are neither frozen nor hot
+			});
+
+			var inactivePrey = players.filter(player => {
+				return player.isFrozen && !player.isHot;
+			})
+
+			switch(options.ruleset)
+			{
+				case "lastman":
+					if(activePrey.length > 0)
+					{
+						gameResults.winners = activePrey;
+						gameResults.losers = activePredators.concat(inactivePrey);
+					}
+					else
+					{
+						gameResults.winners = activePredators;
+						gameResults.losers = activePrey.concat(inactivePrey);
+					}
+					
+					break;
+
+				case "infection":
+					if(activePrey.length > 0)
+					{
+						gameResults.winners = activePrey;
+						gameResults.losers = activePredators;
+					}
+					else
+					{
+						gameResults.winners = activePredators;
+						gameResults.losers = activePrey;
+					}
+					break;
+			}
+
+			console.log(gameResults);
+
+			// save in the db here //
 		}
 
-		console.log(gameResults);
-
-		// save in the db here //
-	}
-
-};
+	};
+}());
